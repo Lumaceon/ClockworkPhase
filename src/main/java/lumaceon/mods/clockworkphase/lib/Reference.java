1a8792d70c9b64853bac0ce6a2cf2f756145adb7
Fluffies <Azuremeadow@gmail.com>
2014-11-25 11:50:44 -0500

    Actual first release. Underscores look so much better.

--- src/main/java/lumaceon/mods/clockworkphase/lib/Reference.java
+++ src/main/java/lumaceon/mods/clockworkphase/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "clockworkphase";
     public static final String MOD_NAME = "ClockworkPhase";
-    public static final String VERSION = "1.7.10-1.0";
+    public static final String VERSION = "1.7.10_1.0";
     public static final String CLIENT_PROXY = "lumaceon.mods.clockworkphase.proxy.ClientProxy";
     public static final String SERVER_PROXY = "lumaceon.mods.clockworkphase.proxy.ServerProxy";
 }
