8da802f3735ab071592a502307c43f46b35dbfc4
Fluffies <Azuremeadow@gmail.com>
2015-01-05 12:14:15 -0500

    Major additions to the configuration file. Disabled phase events for now.

--- src/main/java/lumaceon/mods/clockworkphase/config/ConfigurationHandler.java
+++ src/main/java/lumaceon/mods/clockworkphase/config/ConfigurationHandler.java
@@ -11,14 +11,53 @@ public class ConfigurationHandler
     public static void init(File configurationFile)
     {
         Configuration config = new Configuration(configurationFile);
+        String CATEGORY_HOURGLASS = "hourglasses";
+        String CATEGORY_TENSION = "generalTension";
+        String CATEGORY_EXTRACTORS = "extractors";
+        String CATEGORY_MODULES = "pocketWatchModules";
+        String CATEGORY_TOOLS = "toolTimeSandIncrements";
 
         try
         {
             config.load();
 
             //Add each configuration value here
-            MechanicTweaker.PHASE_EVENTS = config.get(Configuration.CATEGORY_GENERAL, "EnablePhaseEvents", false, "Enables or disables world-wide events that occur occasionally based on phases.").getBoolean(false);
+            //MechanicTweaker.PHASE_EVENTS = config.get(Configuration.CATEGORY_GENERAL, "EnablePhaseEvents", false, "Enables or disables world-wide events that occur occasionally based on phases.").getBoolean(false);
             GlobalPhaseReference.phaseDuration = config.get(Configuration.CATEGORY_GENERAL, "PhaseDuration", 24000, "The duration of each phase in ticks. 1 Minecraft day = 24,000.").getInt(24000);
+
+            MechanicTweaker.MAX_TENSION = config.get(CATEGORY_TENSION, "MainspringMaxTension", 1000000, "The maximum amount of tension a mainspring can hold").getInt(1000000);
+            MechanicTweaker.TENSION_PER_BLOCK_BREAK = config.get(CATEGORY_TENSION, "TensionPerBlockBroken", 250, "The base (1:1 speed/quality) tension cost for each block broken with clockwork tools.").getInt(250);
+            MechanicTweaker.TENSION_PER_HIT = config.get(CATEGORY_TENSION, "TensionPerEntityHit", 500, "The base (1:1 speed/quality) tension cost for each entity attacked with the clockwork sword.").getInt(250);
+            MechanicTweaker.CLOCKWORK_ARMOR_TENSION_COST = config.get(CATEGORY_TENSION, "CWArmorBaseTensionCost", 250, "The base (1:1 speed/quality) tension cost for each piece of clockwork armor on hit.").getInt(250);
+
+            MechanicTweaker.LIFE_HOURGLASS_TENSION_COST = config.get(CATEGORY_HOURGLASS, "LifeHourglassTensionCost", 500, "The base (1:1 speed/quality) tension cost for each tick the hourglass is active.").getInt(500);
+            MechanicTweaker.LIGHT_HOURGLASS_TENSION_COST = config.get(CATEGORY_HOURGLASS, "LightHourglassTensionCost", 500, "The base (1:1 speed/quality) tension cost for each light the hourglass places.").getInt(500);
+            MechanicTweaker.WATER_HOURGLASS_TENSION_COST = config.get(CATEGORY_HOURGLASS, "WaterHourglassTensionCost", 80, "The base (1:1 speed/quality) tension cost for each tick the hourglass is active.").getInt(80);
+            MechanicTweaker.EARTH_HOURGLASS_TENSION_COST = config.get(CATEGORY_HOURGLASS, "EarthHourglassTensionCost", 20, "The base (1:1 speed/quality) tension cost for each tick the hourglass is active.").getInt(20);
+            MechanicTweaker.AIR_HOURGLASS_TENSION_COST = config.get(CATEGORY_HOURGLASS, "AirHourglassTensionCost", 50, "The base (1:1 speed/quality) tension cost for each tick the hourglass is active.").getInt(50);
+            MechanicTweaker.FIRE_HOURGLASS_TENSION_COST = config.get(CATEGORY_HOURGLASS, "FireHourglassTensionCost", 120, "The base (1:1 speed/quality) tension cost for each tick the hourglass is active.").getInt(120);
+            MechanicTweaker.LUNAR_HOURGLASS_TENSION_COST = config.get(CATEGORY_HOURGLASS, "LunarHourglassTensionCost", 50, "The base (1:1 speed/quality) tension cost for each tick the hourglass is active.").getInt(50);
+            MechanicTweaker.DEATH_HOURGLASS_TENSION_COST = config.get(CATEGORY_HOURGLASS, "DeathHourglassTensionCost", 250, "The base (1:1 speed/quality) tension cost for each tick the hourglass is active.").getInt(250);
+
+            MechanicTweaker.TIME_SAND_COST_SILK_MODULE = config.get(CATEGORY_MODULES, "SilkTouchModuleCost", 25, "The time sand cost for each block silk-touched.").getInt(25);
+            MechanicTweaker.TIME_SAND_COST_FURNACE_MODULE = config.get(CATEGORY_MODULES, "FurnaceModuleCost", 25, "The time sand cost for each block auto-smelted.").getInt(25);
+            MechanicTweaker.TIME_SAND_COST_LIFE_WALK_MODULE = config.get(CATEGORY_MODULES, "LifeWalkModuleCost", 3, "The time sand cost for each 'Life energy' which, by default, is 1% of a full heart (2hp).").getInt(3);
+            MechanicTweaker.TIME_SAND_COST_PART_GRAVITY_MODULE = config.get(CATEGORY_MODULES, "PartialGravityModuleCost", 2, "The time sand cost for each tick of partial gravity.").getInt(2);
+            MechanicTweaker.TIME_SAND_COST_DEATH_WALK_MODULE = config.get(CATEGORY_MODULES, "DeathWalkModuleCost", 10, "The time sand cost for each 'Death energy' which, by default, is 1% of a full heart (2hp).").getInt(10);
+
+            MechanicTweaker.PICKAXE_TIME_SAND_INCREMENT = config.get(CATEGORY_TOOLS, "PickaxeTimeSandIncrement", 25, "The amount of time sand the pickaxe extracts from a valid broken block on a successful roll.").getInt(25);
+            MechanicTweaker.AXE_TIME_SAND_INCREMENT = config.get(CATEGORY_TOOLS, "AxeTimeSandIncrement", 100, "The amount of time sand the axe extracts from a valid broken block on a successful roll.").getInt(100);
+            MechanicTweaker.SHOVEL_TIME_SAND_INCREMENT = config.get(CATEGORY_TOOLS, "ShovelTimeSandIncrement", 20, "The amount of time sand the shovel extracts from a valid broken block on a successful roll.").getInt(20);
+            MechanicTweaker.SABER_TIME_SAND_INCREMENT_KILL = config.get(CATEGORY_TOOLS, "SwordTimeSandIncrement", 500, "The amount of time sand the sword extracts from a slain entity on a successful roll.").getInt(500);
+
+            MechanicTweaker.TIME_SAND_FROM_NATURAL_SPAWN = config.get(CATEGORY_EXTRACTORS, "ExtractorLifeTimeSandFromSpawn", 5000, "The amount of time sand extracted from a nearby natural spawn.").getInt(5000);
+            MechanicTweaker.TIME_SAND_FROM_LIGHT_SECOND = config.get(CATEGORY_EXTRACTORS, "ExtractorLightTimeSandPerTick", 100, "The amount of time sand extracted per tick if nearby area is fully lit.").getInt(100);
+            MechanicTweaker.TIME_SAND_FROM_WATER_SECOND = config.get(CATEGORY_EXTRACTORS, "ExtractorWaterTimeSandPerTick", 100, "The amount of time sand extracted per tick if nearby area is all water.").getInt(100);
+            MechanicTweaker.TIME_SAND_FROM_TREE_EXTRACTION = config.get(CATEGORY_EXTRACTORS, "ExtractorEarthTimeSandGrowth", 500, "The amount of time sand extracted from a nearby tree growth.").getInt(500);
+            MechanicTweaker.TIME_SAND_FROM_AIR_SECOND = config.get(CATEGORY_EXTRACTORS, "ExtractorAirTimeSandPerTick", 100, "The amount of time sand extracted per tick if the extractor is at max height.").getInt(100);
+            MechanicTweaker.TIME_SAND_FROM_ONE_FIRE_DAMAGE = config.get(CATEGORY_EXTRACTORS, "ExtractorFireTimeSandFromFireDamage", 300, "The amount of time sand extracted from a nearby entity hurt by fire.").getInt(300);
+            MechanicTweaker.TIME_SAND_FROM_MOON_SECOND = config.get(CATEGORY_EXTRACTORS, "ExtractorLunarTimeSandFromSpawn", 100, "The amount of time sand extracted per tick if nighttime.").getInt(100);
+            MechanicTweaker.TIME_SAND_FROM_DEATH = config.get(CATEGORY_EXTRACTORS, "ExtractorDeathTimeSandFromDeath", 2000, "The amount of time sand extracted from a nearby death.").getInt(2000);
         }
         catch(Exception e)
         {
--- src/main/java/lumaceon/mods/clockworkphase/lib/MechanicTweaker.java
+++ src/main/java/lumaceon/mods/clockworkphase/lib/MechanicTweaker.java
@@ -2,44 +2,44 @@ package lumaceon.mods.clockworkphase.lib;
 
 public class MechanicTweaker
 {
-    public static boolean PHASE_EVENTS = true;
+    public static boolean PHASE_EVENTS = false;
 
-    public static int MAINSPRING_TENSION_MUILTIPLIER = 8;
+    public static final int MAINSPRING_TENSION_MUILTIPLIER = 8;
 
-    public static final int MAX_TENSION = 1000000;
+    public static int MAX_TENSION = 1000000;
     public static final int MAX_TS_DEFAULT = 1000000;
-    public static final int MAX_TIME_SAND_TOOLS = 10000;
+    public static int MAX_TIME_SAND_TOOLS = 10000;
     public static final int MAX_TS_TIME_WELL = 100000000;
     public static final int MAX_TS_EXTRACTORS = 1000000;
 
-    public static final int TIME_SAND_FROM_NATURAL_SPAWN = 5000; //Life
-    public static final int TIME_SAND_FROM_LIGHT_SECOND = 100; //Light
-    public static final int TIME_SAND_FROM_WATER_SECOND = 100; //Water
-    public static final int TIME_SAND_FROM_TREE_EXTRACTION = 500; //Earth
-    public static final int TIME_SAND_FROM_AIR_SECOND = 100; //Air
-    public static final int TIME_SAND_FROM_ONE_FIRE_DAMAGE = 300; //Fire
-    public static final int TIME_SAND_FROM_MOON_SECOND = 100; //Lunar
-    public static final int TIME_SAND_FROM_DEATH = 2000; //Death
+    public static int TIME_SAND_FROM_NATURAL_SPAWN = 5000; //Life
+    public static int TIME_SAND_FROM_LIGHT_SECOND = 100; //Light
+    public static int TIME_SAND_FROM_WATER_SECOND = 100; //Water
+    public static int TIME_SAND_FROM_TREE_EXTRACTION = 500; //Earth
+    public static int TIME_SAND_FROM_AIR_SECOND = 100; //Air
+    public static int TIME_SAND_FROM_ONE_FIRE_DAMAGE = 300; //Fire
+    public static int TIME_SAND_FROM_MOON_SECOND = 100; //Lunar
+    public static int TIME_SAND_FROM_DEATH = 2000; //Death
 
-    public static final int PICKAXE_TIME_SAND_INCREMENT = 25;
-    public static final int AXE_TIME_SAND_INCREMENT = 100;
-    public static final int SHOVEL_TIME_SAND_INCREMENT = 20;
-    public static final int SABER_TIME_SAND_INCREMENT_KILL = 500;
+    public static int PICKAXE_TIME_SAND_INCREMENT = 25;
+    public static int AXE_TIME_SAND_INCREMENT = 100;
+    public static int SHOVEL_TIME_SAND_INCREMENT = 20;
+    public static int SABER_TIME_SAND_INCREMENT_KILL = 500;
 
 
-    public static final int LIFE_HOURGLASS_TENSION_COST = 500; //Once per tick.
-    public static final int LIGHT_HOURGLASS_TENSION_COST = 500; //Once per light.
-    public static final int WATER_HOURGLASS_TENSION_COST = 80; //Once per tick.
-    public static final int EARTH_HOURGLASS_TENSION_COST = 20; //Once per tick.
-    public static final int AIR_HOURGLASS_TENSION_COST = 50; //Once per tick.
-    public static final int FIRE_HOURGLASS_TENSION_COST = 120; //Once per tick.
-    public static final int LUNAR_HOURGLASS_TENSION_COST = 50; //Once per tick.
-    public static final int DEATH_HOURGLASS_TENSION_COST = 250; //Once per tick.
+    public static int LIFE_HOURGLASS_TENSION_COST = 500; //Once per tick.
+    public static int LIGHT_HOURGLASS_TENSION_COST = 500; //Once per light.
+    public static int WATER_HOURGLASS_TENSION_COST = 80; //Once per tick.
+    public static int EARTH_HOURGLASS_TENSION_COST = 20; //Once per tick.
+    public static int AIR_HOURGLASS_TENSION_COST = 50; //Once per tick.
+    public static int FIRE_HOURGLASS_TENSION_COST = 120; //Once per tick.
+    public static int LUNAR_HOURGLASS_TENSION_COST = 50; //Once per tick.
+    public static int DEATH_HOURGLASS_TENSION_COST = 250; //Once per tick.
 
-    public static final int CLOCKWORK_ARMOR_TENSION_COST = 250; //Once per hit (Applies to each piece of armor).
+    public static int CLOCKWORK_ARMOR_TENSION_COST = 250; //Once per hit (Applies to each piece of armor).
 
-    public static final int TENSION_PER_BLOCK_BREAK = 250; //For clockwork tools, applies once each time a block is broken.
-    public static final int TENSION_PER_HIT = 500; //For clockwork saber, applies once each time an entity is hit.
+    public static int TENSION_PER_BLOCK_BREAK = 250; //For clockwork tools, applies once each time a block is broken.
+    public static int TENSION_PER_HIT = 500; //For clockwork saber, applies once each time an entity is hit.
     public static final int TIME_SAND_PER_BLOCK_BREAK_PICKAXE = PICKAXE_TIME_SAND_INCREMENT; //For temporal pickaxe, applies once each time a block is broken.
     public static final int TIME_SAND_PER_BLOCK_BREAK_AXE = AXE_TIME_SAND_INCREMENT; //For temporal axe, applies once each time a block is broken.
     public static final int TIME_SAND_PER_BLOCK_BREAK_SHOVEL = SHOVEL_TIME_SAND_INCREMENT; //For temporal shovel, applies once each time a block is broken.
@@ -56,11 +56,11 @@ public class MechanicTweaker
     public static final int TIME_SAND_COST_TEMPORAL_SHOVEL = 30; //1.5 Chips worth.
 
     /** Pocket Watch Module Time Sand Costs **/
-    public static final int TIME_SAND_COST_SILK_MODULE = 25; //Silk touch module, once per block silk-touched.
-    public static final int TIME_SAND_COST_FURNACE_MODULE = 25; //Furnace module, once per block auto-smelted.
-    public static final int TIME_SAND_COST_LIFE_WALK_MODULE = 3; //Life walk module, amount of time sand per health power.
-    public static final int TIME_SAND_COST_PART_GRAVITY_MODULE = 2; //Lunar boots, amount of time sand per active tick.
-    public static final int TIME_SAND_COST_DEATH_WALK_MODULE = 10; //Death walk module, amount of time sand per death power.
+    public static int TIME_SAND_COST_SILK_MODULE = 25; //Silk touch module, once per block silk-touched.
+    public static int TIME_SAND_COST_FURNACE_MODULE = 25; //Furnace module, once per block auto-smelted.
+    public static int TIME_SAND_COST_LIFE_WALK_MODULE = 3; //Life walk module, amount of time sand per health power.
+    public static int TIME_SAND_COST_PART_GRAVITY_MODULE = 2; //Lunar boots, amount of time sand per active tick.
+    public static int TIME_SAND_COST_DEATH_WALK_MODULE = 10; //Death walk module, amount of time sand per death power.
 
     /** Pocket Watch Module Mechanics **/
     public static final int LIFE_DEFENSE_MAX = 1000; //Life walk module, max life energy it can hold.
