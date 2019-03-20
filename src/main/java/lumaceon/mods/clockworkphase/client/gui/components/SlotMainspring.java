d10629b90aa2069f4a6c9c29dc0552d13470771a
FTB_lag <dashotmine@ro.ru>
2019-03-20 18:38:12 +0300

    Try fix bug with Sponge.

--- src/main/java/lumaceon/mods/clockworkphase/client/gui/components/SlotMainspring.java
+++ src/main/java/lumaceon/mods/clockworkphase/client/gui/components/SlotMainspring.java
@@ -1,20 +1,45 @@
 package lumaceon.mods.clockworkphase.client.gui.components;
 
 import lumaceon.mods.clockworkphase.init.ModItems;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.inventory.Slot;
+import net.minecraft.inventory.SlotCrafting;
 import net.minecraft.item.ItemStack;
 
-public class SlotMainspring extends Slot
-{
-    public SlotMainspring(IInventory inventory, int id, int x, int y)
-    {
-        super(inventory, id, x, y);
+public class SlotMainspring extends SlotCrafting {
+
+    public SlotMainspring(IInventory inventoryIn, int slotIndex, int xPosition, int yPosition) {
+        super(null, null, inventoryIn, slotIndex, xPosition, yPosition);
     }
 
     @Override
-    public boolean isItemValid(ItemStack is)
-    {
-        return is.getItem().equals(ModItems.mainspring);
+    public boolean isItemValid(ItemStack stack) {
+        return stack.getItem().equals(ModItems.mainspring);
+    }
+
+    @Override
+    protected void onCrafting(ItemStack stack) {
+    }
+
+    @Override
+    public ItemStack onTake(EntityPlayer thePlayer, ItemStack stack) {
+        this.onSlotChanged();
+        return stack;
     }
 }
+
+//public class SlotMainspring extends Slot
+//{
+//    public SlotMainspring(IInventory inventory, int id, int x, int y)
+//    {
+//        super(inventory, id, x, y);
+//    }
+//
+//    @Override
+//    public boolean isItemValid(ItemStack is)
+//    {
+//        return is.getItem().equals(ModItems.mainspring);
+//    }
+//}
--- src/main/java/lumaceon/mods/clockworkphase/inventory/InventoryMainspringAssembly.java
+++ src/main/java/lumaceon/mods/clockworkphase/inventory/InventoryMainspringAssembly.java
@@ -3,81 +3,100 @@ package lumaceon.mods.clockworkphase.inventory;
 import lumaceon.mods.clockworkphase.custom.IInventoryHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.inventory.ItemStackHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 
-public class InventoryMainspringAssembly implements IInventoryHelper
-{
+public class InventoryMainspringAssembly extends InventoryCrafting {
 
-    private NonNullList<ItemStack> inventory;
-    private Container eventHandler;
+    private final Container container;
 
-    public InventoryMainspringAssembly(Container eventHandler)
-    {
-        inventory = NonNullList.withSize(8, ItemStack.EMPTY);
-        this.eventHandler = eventHandler;
+    public InventoryMainspringAssembly(Container eventHandlerIn) {
+        super(eventHandlerIn, 4, 2);
+        container = eventHandlerIn;
     }
 
     @Override
-    public NonNullList<ItemStack> getInv() {
-        return inventory;
-    }
-
-    @Override
-    public ItemStack decrStackSize(int index, int lossCount)
-    {
-        if (!inventory.get(index).isEmpty())
-        {
-            ItemStack itemstack;
-
-            if (inventory.get(index).getCount() <= lossCount)
-            {
-                itemstack = inventory.get(index);
-                inventory.set(index, ItemStack.EMPTY);
-                this.eventHandler.onCraftMatrixChanged(this);
-                return itemstack;
-            }
-            else
-            {
-                itemstack = inventory.get(index).splitStack(lossCount);
-
-                if (inventory.get(index).getCount() == 0)
-                {
-                    inventory.set(index, ItemStack.EMPTY);
-                }
-
-                this.eventHandler.onCraftMatrixChanged(this);
-                return itemstack;
-            }
-        }
-        else
-        {
-            return ItemStack.EMPTY;
-        }
-    }
-
-    @Override
-    public void setInventorySlotContents(int p_70299_1_, ItemStack p_70299_2_)
-    {
-        inventory.set(p_70299_1_, p_70299_2_);
-        this.eventHandler.onCraftMatrixChanged(this);
-    }
-
-    @Override
-    public int getInventoryStackLimit() { return 64; }
-
-    @Override
-    public void markDirty() {}
-
-    @Override
-    public boolean isUsableByPlayer(EntityPlayer p_70300_1_) { return true; }
-
-    @Override
-    public boolean isItemValidForSlot(int p_94041_1_, ItemStack p_94041_2_) { return false; }
-
-    @Override
-    public ITextComponent getDisplayName() {
-        return null;
+    public ItemStack decrStackSize(int index, int count) {
+        ItemStack stack = super.decrStackSize(index, count);
+        this.container.onCraftMatrixChanged(this);
+        return stack;
     }
 }
+
+//public class InventoryMainspringAssembly implements IInventoryHelper
+//{
+//
+//    private NonNullList<ItemStack> inventory;
+//    private Container eventHandler;
+//
+//    public InventoryMainspringAssembly(Container eventHandler)
+//    {
+//        inventory = NonNullList.withSize(8, ItemStack.EMPTY);
+//        this.eventHandler = eventHandler;
+//    }
+//
+//    @Override
+//    public NonNullList<ItemStack> getInv() {
+//        return inventory;
+//    }
+//
+//    @Override
+//    public ItemStack decrStackSize(int index, int lossCount)
+//    {
+//        if (!inventory.get(index).isEmpty())
+//        {
+//            ItemStack itemstack;
+//
+//            if (inventory.get(index).getCount() <= lossCount)
+//            {
+//                itemstack = inventory.get(index);
+//                inventory.set(index, ItemStack.EMPTY);
+//                this.eventHandler.onCraftMatrixChanged(this);
+//                return itemstack;
+//            }
+//            else
+//            {
+//                itemstack = inventory.get(index).splitStack(lossCount);
+//
+//                if (inventory.get(index).getCount() == 0)
+//                {
+//                    inventory.set(index, ItemStack.EMPTY);
+//                }
+//
+//                this.eventHandler.onCraftMatrixChanged(this);
+//                return itemstack;
+//            }
+//        }
+//        else
+//        {
+//            return ItemStack.EMPTY;
+//        }
+//    }
+//
+//    @Override
+//    public void setInventorySlotContents(int p_70299_1_, ItemStack p_70299_2_)
+//    {
+//        inventory.set(p_70299_1_, p_70299_2_);
+//        this.eventHandler.onCraftMatrixChanged(this);
+//    }
+//
+//    @Override
+//    public int getInventoryStackLimit() { return 64; }
+//
+//    @Override
+//    public void markDirty() {}
+//
+//    @Override
+//    public boolean isUsableByPlayer(EntityPlayer p_70300_1_) { return true; }
+//
+//    @Override
+//    public boolean isItemValidForSlot(int p_94041_1_, ItemStack p_94041_2_) { return false; }
+//
+//    @Override
+//    public ITextComponent getDisplayName() {
+//        return null;
+//    }
+//}
